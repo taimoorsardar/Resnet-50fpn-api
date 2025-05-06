830ad3d9ce1c52970a2abca678a4b2fb7caf235d
Taimoor Sardar <tsardar.bese21seecs@seecs.edu.pk>
2025-05-06 15:12:08 +0500

    fixed port

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ def health():
     return {"status": "ok"}
 if __name__ == "__main__":
     # Run the API with uvicorn: python main.py
-    port = int(os.environ.get["PORT",8080])
+    port = int(os.environ.get["PORT",8000])
     uvicorn.run(app, host="0.0.0.0", port=port)
